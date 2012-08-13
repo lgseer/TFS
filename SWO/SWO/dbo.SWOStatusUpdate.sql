70e196f5f4031cacd29f8eae7c160236d64e872e
Sudheer <lgseer@gmail.com>
2012-08-13 22:02:40 +0530

    revise

--- SWO/SWO/dbo.SWOStatusUpdate.sql
+++
@@ -1,8 +0,0 @@
-DECLARE @SWOID int = 12
-DECLARE @valMessages table(id int, Message varchar(100))
-
-select SA.SWOID, SA.SWOActivityID, sa.code, SA.Description,
-from tblSWO S
-  left join tblswoActivities  SA on SA.SWOID = S.SWOID
-  left join tblResourceEngagement RE on RE.ActivityID = SA.SWOActivityID
-where S.SWOID = @SWOID
